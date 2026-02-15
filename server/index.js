3290e1e93b9818b0656684ce85e1b5d1c998ed21
abusufian_ui <ranasuffyan9@gmail.com>
2026-02-15 15:23:08 +0500

    Fixed port for Render

--- server/index.js
+++ server/index.js
@@ -523,7 +523,7 @@ app.get('/', (req, res) => {
   res.json({ message: "API is running 🚀" });
 });
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
