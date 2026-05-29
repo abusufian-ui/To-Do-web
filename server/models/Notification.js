f4075a4f760887733e79212c0a0191ed1245bc92
abusufian_ui <ranasuffyan9@gmail.com>
2026-05-29 18:21:34 +0500

    Broadcast web bug fixed

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -9,7 +9,7 @@ const notificationSchema = new mongoose.Schema({
   type: {
     type: String,
     required: true,
-    enum: ['task', 'group', 'note', 'system', 'keynote', 'general', 'marks', 'attendance', 'submission', 'announcement']
+    enum: ['task', 'group', 'note', 'system', 'keynote', 'general', 'marks', 'attendance', 'submission', 'announcement', 'broadcast']
   },
   title: {
     type: String,
